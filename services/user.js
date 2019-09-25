36ba39aac1d0444b0b1fb27c6b54b4895d6f275f
Diego van Haaster <diegomvh@gmail.com>
2019-09-25 20:09:26 -0300

    Services

--- services/user.js
+++ services/user.js
@@ -1,39 +1,24 @@
 const models = require('../models')
 let User = models.User;
 
-class UserService{
-    constructor(){
-
-    }
-    getUsers(){
-        // comentario
-    }
-    findUser(user){
-        //find
-        const {email,password} = user.body;
-        return Promise.resolve(
-            User.findOne({
-                where:{
-                    email
-                }
-            }).then((data) =>{
-                // solo el objeto
-                console.log(data);
-                return data.dataValues; 
-            }).catch((err)=>{
-                console.log("sale mal:*******************"+err);
-            })
-        );
-    }
-    createUser(user){
-        const { email, password } = user.body;
-        return Promise.resolve(
-            User.create({
-                email,
-                password
-            })
-        );
-    }
+module.exports = {
+  getUsers() {
+    // comentario
+  },
+  findUser(email) { 
+    return User.findOne({
+      where: {
+        email
+      }
+    }).then(data => data.dataValues);
+  },
+  createUser(user) {
+    const { email, password } = user.body;
+    return Promise.resolve(
+      User.create({
+        email,
+        password
+      })
+    );
+  }
 }
-
-module.exports = UserService;
