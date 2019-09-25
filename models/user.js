6b4309b052f2230105b17454de1c66f3bdc16f42
danibarria <barriareyesjosedaniel@gmail.com>
2019-09-25 13:11:04 -0300

    wip users login

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 'use strict';
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) =>{
     const User = sequelize.define('User',{
         email:DataTypes.STRING,
         password: DataTypes.STRING
-    })
+    },{
+        hooks:{
+            afterValidate: (user) => {
+                user.password = bcrypt.hashSync(user.password,10);
+            }
+
+        }
+    });
+
     User.associate = function(models){
         models.User.belongsTo(models.Persona);
         models.User.belongsToMany(models.Rol, {
