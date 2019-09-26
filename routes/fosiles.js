0754ddd24c9f4ad55143a666a1e0944ddad5734a
danibarria <barriareyesjosedaniel@gmail.com>
2019-09-25 22:53:46 -0300

    refactor fosil

--- routes/fosiles.js
+++ routes/fosiles.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const FosilService = require('../services/fosil')
-
-let fosilService = new FosilService();
+const fosilService = require('../services/fosil')
 
 router.get('/', (req, res, next) => {
     fosilService.getFosiles()
