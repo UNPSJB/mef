2d1bc5e28a1cc282ca2eaf9806a08dade3340391
danibarria <barriareyesjosedaniel@gmail.com>
2019-09-25 22:51:57 -0300

    refactor de fosil

--- routes/fosiles.js
+++ routes/fosiles.js
@@ -5,27 +5,22 @@ const FosilService = require('../services/fosil')
 let fosilService = new FosilService();
 
 router.get('/', (req, res, next) => {
-    let results = null;
     fosilService.getFosiles()
-        .then((fosiles) => {
-            results = fosiles.map((row) => {
-                console.log(row);
-                return row.dataValues;
-            });
-            console.log(results);
+        .then((results) => {
             res.render('fosil', {
                 results
             });
         });
 });
+
 router.get('/agregarFosil', (req, res, next) => {
     res.render('agregarFosil'); 
 });
+
 router.post('/', (req, res, next)=>{
-    fosilService.createFosil(req.body)
-    .then(()=>{
-        res.redirect('/fosiles');
-    })
+    const {numero_coleccion,peso,disponible,fecha_encontrado,observacion} = req.body;
+    fosilService.createFosil(numero_coleccion,peso,disponible,fecha_encontrado,observacion)
+    .then(()=>{res.redirect('/fosiles')})
 });
 
 module.exports = router;
