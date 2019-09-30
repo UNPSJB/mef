93b5195be8d115c5dabe03175b0a7654011a4882
alex_99 <alex_villanuevasoto@hotmail.com>
2019-09-30 20:00:46 -0300

    Se agrego hueso sin terminar

--- models/hueso.js
+++ models/hueso.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Hueso = sequelize.define('Hueso', {
     nombre: DataTypes.STRING,
+    numero: DataTypes.INTEGER,
     subtipohueso: {
       type: DataTypes.ENUM,
       values: ['Torax','Vertebral','Craneo','Pelvis','Brazo','Manos','Piernas','Pies']
@@ -9,6 +10,17 @@ module.exports = (sequelize, DataTypes) => {
     tipohueso: {
       type: DataTypes.ENUM,
       values: ['Apendicular','Axial']
+    },
+    disponible: {
+      type: DataTypes.ENUM,
+      values: ['SI','NO']
+    },
+    DinosaurioId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Dinosaurios',//nombre en la tabla
+        key:'id',        // id de la tabla SubClases
+      }
     }
   }, {});
   Hueso.associate = function(models) {
--- routes/dinosaurios.js
+++ routes/dinosaurios.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dinoService = require('../services/dinosaurio');
+const huesoService = require('../services/hueso');
 
 router.get('/', (req, res, next) => {
   dinoService.getDinosaurios()
@@ -46,6 +47,68 @@ router.put('/', (req,res,next)=>{
 router.delete('/' , (req,res,next) =>{
   dinoService.deleteDinosaurio(req.body.id)
     .then(() => res.redirect('/dinosaurios'));
+});
+
+// HUESOS
+
+router.get('/huesos', (req,res,next) => {
+  res.render('huesos/hueso',{
+    jefeexhibicion:true});
+});
+
+router.post('/huesos', (req, res) => {
+  let { 
+    cervicales,
+    dorsales,
+    sacras,
+    caudales,
+    cos_cervicales,
+    cos_dorsales,
+    hemales,
+    metacarpianos,
+    metatarsos,
+    dedos_mano,
+    dedos_pata } = req.body;
+  const huesos  = [
+      "Vertebra Cervical",
+      "Vertebra Dorsal",
+      "Vertebra Sacra",
+      "Vertebra Caudal",
+      "Costilla Cervical izquierda",
+      "Costilla Cervical derecha",
+      "Costilla Dorsal izquierda",
+      "Costilla Dorsal derecha",
+      "Hemal",
+      "Metacarpiano izquierdo",
+      "Metacarpiano derecho",
+      "Metatarso izquierdo",
+      "Metatarso derecho",
+      "Falange de mano izquierda",
+      "Falange de mano derecha",
+      "Falange de pie izquierda",
+      "Falange de pie derecha"
+  ];
+  let hueso_loco = huesos.map((item)=>{
+    if (item.startsWith("Vertebra")){
+      return [ item, "Vertebra",'NO'];
+    }
+    if (item.startsWith("Costilla")){
+      return [ item, "Tórax",'NO'];
+    }
+    if (item.startsWith("Metacarpiano")){
+      return [ item, "Mano",'NO'];
+    }
+    if (item.startsWith("Metatarso")){
+      return [ item, "Pie",'NO'];
+    }
+    if (item.startsWith("Falange de mano")){
+      return [ item, "Mano",'NO'];
+    }
+    if (item.startsWith("Falange de pie")){
+      return [ item, "Pie",'NO'];
+    }
+  });
+  console.log(hueso_loco);
 });
 
 module.exports = router;
