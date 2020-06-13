bf0e71c9c7e7220de52c0d44eec4af836acaf1b3
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-13 17:02:43 -0300

    paginado de dinosaurio

--- routes/dinosaurios.js
+++ routes/dinosaurios.js
@@ -1,127 +1,145 @@
 const express = require('express');
 const router = express.Router();
-const permisos = require('../auth/permisos');
+const permisos = require('../middlewares/permisos');
 const dinoService = require('../services/dinosaurio');
 const huesoService = require('../services/hueso');
 const subclaseService = require('../services/subclase');
+
+const { generatePagination } = require('../services/utils')
+const paginate = require('../middlewares/paginate')
+
 
 router.get('/', 
   permisos.permisoPara([
     permisos.ROLES.TALLER, 
     permisos.ROLES.COLECCION,
-    permisos.ROLES.EXHIBICION]), (req, res, next) => {/// TALLER accede aca
-  dinoService.getDinosaurios()
-    .then((results) => {
-      res.render('dinosaurios/dinosaurio', {
-        results,req
+    permisos.ROLES.EXHIBICION]), 
+  paginate,
+  async (req, res) => {
+    try {
+      const { page, limit } = req.query
+      const dinosaurio = await dinoService.getDinosaurios(page, limit)
+      res.render('dinosaurios/dinosaurio', { dinosaurio: dinosaurio.rows, ...generatePagination(dinosaurio.count, page, limit), req })
+    } catch (error) {
+      console.log(error)
+    }
+  })
+  
+  router.get('/agregar',
+  permisos.permisoPara([permisos.ROLES.COLECCION]),
+  async (req, res) => {
+    try {
+      const subclases = await subclaseService.getSubclases()
+      res.render('dinosaurios/agregar',{
+        subclases,req
       })
-    })
-});
-
-router.get('/agregar',
-permisos.permisoPara([permisos.ROLES.COLECCION]),
-(req,res,next) => {
-  subclaseService.getSubclases()
-  .then((subclases)=>{
-    res.render('dinosaurios/agregar',{
-      subclases,req
-    })
+    } catch (error) {
+      console.log(error)
+    }
   });
-});
-
-router.get('/editar/:id', 
-permisos.permisoPara([permisos.ROLES.COLECCION]),
-async (req,res,next) => {
-  //ver cuando id no existe
-  const dino = await dinoService.getDinosaurio(req.params.id);
-  subclaseService.getSubclases()
-  .then((subclases)=>{
-    res.render('dinosaurios/editar', { dino, subclases,req });    
-  }); //@TODO mostrar dino sin editar o algo
+  
+  router.get('/editar/:id', 
+    permisos.permisoPara([permisos.ROLES.COLECCION]),
+    async (req, res) => {
+      try {
+        const dino = await dinoService.getDinosaurio(req.params.id);
+        const subclases = await subclaseService.getSubclases()
+        res.render('dinosaurios/editar', { dino, subclases,req });    
+      } catch (error) {
+        console.log(error)
+      }
 });
 
 router.get('/eliminar/:id', 
-permisos.permisoPara([permisos.ROLES.COLECCION]),
-(req,res,next)=>{
-  dinoService.getDinosaurio(req.params.id)
-  .then((dino)=> {
-    res.render('dinosaurios/eliminar', { dino,req })
-  })
+  permisos.permisoPara([permisos.ROLES.COLECCION]),
+  async (req, res)=>{
+    try {
+      const dino = await dinoService.getDinosaurio(req.params.id)
+      res.render('dinosaurios/eliminar', { dino,req })
+    } catch (error) {
+      console.log(error)      
+    }
 });
 
-// HUESOS
+/** HUESOS  */
+
 router.get('/moldes/:id', 
-permisos.permisoPara([permisos.ROLES.TALLER, permisos.ROLES.COLECCION]),
-(req, res) => { /// TALLER
-  const { id } = req.params;
-  huesoService.getHuesosDino(id)
-    .then((huesos)=>{
+  permisos.permisoPara([permisos.ROLES.TALLER, permisos.ROLES.COLECCION]),
+  async (req, res) => { 
+    try {
+      const { id } = req.params;
+      const huesos = await huesoService.getHuesosDino(id)
+      
       if(req.session.rol === permisos.ROLES.TALLER){
-        const taller = true;
-        res.render("huesos/hueso",{huesos, taller,req});
+        res.render("huesos/hueso", {huesos, taller:true, req});
       }else{
-        res.render("huesos/hueso",{huesos,req});
+        res.render("huesos/hueso", {huesos, req});
       }
-    });
+    } catch (error) {
+      console.log(error)
+    }
 });
 
 router.get('/huesos/:id', 
-permisos.permisoPara([permisos.ROLES.TALLER,permisos.ROLES.COLECCION, permisos.ROLES.EXHIBICION]),
-(req,res)=>{
-  const {id} = req.params;
-  huesoService.getHuesosDino(id)
-    .then((huesos)=>{
-      res.send(JSON.stringify(huesos,null,4))
-    })
+  permisos.permisoPara([permisos.ROLES.TALLER,permisos.ROLES.COLECCION, permisos.ROLES.EXHIBICION]),
+  async (req, res)=>{
+    try {
+      const {id} = req.params;
+      const huesos = await huesoService.getHuesosDino(id)
+      res.send(JSON.stringify(huesos, null, 4))
+    } catch (error) {
+      console.log(error)
+    }
 })
 
 router.patch('/moldes/toggle',
-permisos.permisoPara([permisos.ROLES.TALLER]), 
-(req,res)=>{ /// esto NO PUEDE SER ACCEDIDO por bones
-  const { id } = req.query
-  huesoService.toggleDisponibilidadHueso(id);
-  res.send(200);
+  permisos.permisoPara([permisos.ROLES.TALLER]), 
+  async (req, res)=>{ /// esto NO PUEDE SER ACCEDIDO por bones
+    try {
+      const { id } = req.query
+      huesoService.toggleDisponibilidadHueso(id);
+      res.send(200);
+    } catch (error) {
+      console.log(error)
+    }
 });
 
 router.post('/',
-permisos.permisoPara([permisos.ROLES.COLECCION]), 
-(req,res,next) =>{ // esto llama a dino service
-  const {nombre, alimentacion, periodo, descubrimiento, idsubclase} = req.body;
-  const {cant_cervicales,cant_dorsales,cant_sacras,cant_caudales,cant_cos_cervicales,cant_cos_dorsales,cant_hemales,cant_metacarpianos,cant_metatarsos,cant_dedos_mano,cant_dedos_pata} = req.body;
-  dinoService.createDinosaurio(nombre, alimentacion, periodo, descubrimiento, idsubclase) // es una promesa
-  .then((dinosaurio) => {
-    // createHueso(nombre, numero, DinosaurioId){
-    huesoService.createHuesos(dinosaurio.id, [cant_cervicales,cant_dorsales,cant_sacras,cant_caudales,cant_cos_cervicales,cant_cos_dorsales,cant_hemales,cant_metacarpianos,cant_metatarsos,cant_dedos_mano,cant_dedos_pata]);
-    res.redirect('/dinosaurios'); //@TODO agregar mas experiencia
-  })
-  .catch((errores)=>{
-    const dino = req.body;
-    subclaseService.getSubclases()
-    .then((subclases)=>{
-        res.render("dinosaurios/agregar",{errores,dino,subclases,req})
-    })
-  });
+  permisos.permisoPara([permisos.ROLES.COLECCION]), 
+  async (req, res) =>{ // esto llama a dino service
+    const {nombre, alimentacion, periodo, descubrimiento, idsubclase} = req.body;
+    const {cant_cervicales,cant_dorsales,cant_sacras,cant_caudales,cant_cos_cervicales,cant_cos_dorsales,cant_hemales,cant_metacarpianos,cant_metatarsos,cant_dedos_mano,cant_dedos_pata} = req.body;
+    
+    try {
+      const dinosaurio = await dinoService.createDinosaurio(nombre, alimentacion, periodo, descubrimiento, idsubclase) // es una promesa
+      await huesoService.createHuesos(dinosaurio.id, [cant_cervicales,cant_dorsales,cant_sacras,cant_caudales,cant_cos_cervicales,cant_cos_dorsales,cant_hemales,cant_metacarpianos,cant_metatarsos,cant_dedos_mano,cant_dedos_pata]);
+      res.redirect('/dinosaurios')
+    } catch (error) {
+      const dino = req.body;
+      const subclases = await subclaseService.getSubclases()
+      res.render("dinosaurios/agregar",{errores:error,dino,subclases,req})
+    }
+
 });
 
 router.put('/',
-permisos.permisoPara([permisos.ROLES.COLECCION]), 
-(req,res)=>{
-    dinoService.updateDinosaurio(req.body)
-    .then(() => res.redirect('/dinosaurios'))
-    .catch((errores)=>{
+  permisos.permisoPara([permisos.ROLES.COLECCION]), 
+  async (req, res)=>{
+    try {
+      await dinoService.updateDinosaurio(req.body)
+      res.redirect('/dinosaurios')     
+    } catch (error) {
       const dino = req.body;
-      subclaseService.getSubclases()
-      .then((subclases)=>{
-        res.render("dinosaurios/editar",{errores,dino,subclases,req})
-      })
-    });
+      const subclases = await subclaseService.getSubclases()
+      res.render("dinosaurios/editar",{errores,dino,subclases,req})
+    }
 });
 
 router.delete('/',
-permisos.permisoPara([permisos.ROLES.COLECCION]),
-async (req,res) =>{
-  const { id } = req.body;
-  try {
+  permisos.permisoPara([permisos.ROLES.COLECCION]),
+  async (req, res) =>{
+    const { id } = req.body;
+    try {
       await dinoService.deleteDinosaurio(id)           
     } catch (errores) {
       const dino = await dinoService.getDinosaurio(id);
--- services/dinosaurio.js
+++ services/dinosaurio.js
@@ -1,11 +1,18 @@
 const models = require('../models')
+const { paginateModel } = require('./utils')
 
 module.exports = {
-    getDinosaurios(){//{ tags }//aca se pide datos a la BD        
-        return models.Dinosaurio.findAll({include:[models.SubClase]});        
+    getDinosaurios(page = 0, pageSize = 10, args){//{ tags }//aca se pide datos a la BD        
+        return models.Dinosaurio.findAndCountAll({
+            include:[models.SubClase],
+            where: {
+                ...args
+              },
+            ...paginateModel({ page, pageSize })
+        })        
     },
     getDinosaurio( id ){
-        return models.Dinosaurio.findByPk(id);
+        return models.Dinosaurio.findByPk(id)
     },
     createDinosaurio(nombre, alimentacion, periodo, descubrimiento, SubClaseId){
         return models.Dinosaurio.create({
@@ -14,7 +21,7 @@ module.exports = {
                 periodo,
                 descubrimiento,
                 SubClaseId
-            });
+            })
     },
     updateDinosaurio(dinoReq){
         return models.Dinosaurio.upsert(dinoReq) //update or insert = upsert XD    
@@ -23,7 +30,7 @@ module.exports = {
         return models.Dinosaurio.findByPk(id)
             .then( (dinoEncontrado)=>{
                 //existe ? lo modifico
-                return dinoEncontrado.destroy(dinoEncontrado);
+                return dinoEncontrado.destroy(dinoEncontrado)
             })//dino no existe
     }
 }
