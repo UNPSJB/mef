adfd1f7d6269d7b23b32579bd68ed2ccbd924485
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-13 20:07:59 -0300

    pagination en visitas

--- routes/visitas.js
+++ routes/visitas.js
@@ -1,61 +1,73 @@
-const express = require('express');
-const router = express.Router();
-const visitaService = require('../services/visita');
+const express = require('express')
+const router = express.Router()
+const visitaService = require('../services/visita')
 const exhibicionService = require('../services/exhibicion')
 const clienteService = require('../services/cliente')
 const guiaService = require('../services/guia')
 
+const paginate = require('../middlewares/paginate')
+const { generatePagination } = require("../services/utils")
+
 //lista todos las visitas
-router.get('/', async (req, res) => {
+router.get('/', paginate, async (req, res) => {
+  const { page, limit } = req.query
+
+  try {
+    const countVisitas = await visitaService.countVisitas()
     const visitas = await visitaService.getVisitas()
-    console.log(visitas)
-    res.render('visitas/visita', { results:visitas , req });
-});
+    const paginationObj = {
+      ...generatePagination('visitas', countVisitas, page, limit)
+    }
+    res.render('visitas/visita', { visitas: visitas.rows, paginationObj, req })
+
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 router.get('/agregar', async (req, res) => {
-    const exhibiciones = await exhibicionService.getExhibiciones()
-    const clientes = await clienteService.getClientes()
-    const guias = await guiaService.getGuias()
-    res.render('visitas/agregar',{exhibiciones, clientes, guias, req});
-});
+  const exhibiciones = await exhibicionService.getExhibiciones()
+  const clientes = await clienteService.getClientes()
+  const guias = await guiaService.getGuias()
+  res.render('visitas/agregar', { exhibiciones, clientes, guias, req })
+})
 
 router.get('/editar/:id', async (req, res) => {
-    const { id } = req.params
-    const exhibiciones = await exhibicionService.getExhibiciones()
-    const clientes = await clienteService.getClientes()
-    const guias = await guiaService.getGuias()
-    const visita = await visitaService.getVisita(id)
-    res.render('visitas/editar', { visita, exhibiciones, clientes, guias, req });
+  const { id } = req.params
+  const exhibiciones = await exhibicionService.getExhibiciones()
+  const clientes = await clienteService.getClientes()
+  const guias = await guiaService.getGuias()
+  const visita = await visitaService.getVisita(id)
+  res.render('visitas/editar', { visita, exhibiciones, clientes, guias, req })
 })
 
 router.get('/eliminar/:id', async (req, res) => {
-    const { id } = req.params;
-    const visita = await visitaService.getVisita(id)
-    res.render('visitas/eliminar', { visita, req });
-});
+  const { id } = req.params
+  const visita = await visitaService.getVisita(id)
+  res.render('visitas/eliminar', { visita, req })
+})
 
 router.post('/', async (req, res) => {
-    const {exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio} = req.body
-    const visita = await visitaService.createVisita(exhibicionId,clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
-    res.redirect('/visitas')
-});
+  const { exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body
+  await visitaService.createVisita(exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
+  res.redirect('/visitas')
+})
 
 router.put('/', async (req, res) => {
-    const {id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio} = req.body
+  const { id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body
 
-    return visitaService.updateVisita(id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
-        .then(visita => {
-            console.log('actualizado', visita)
-            res.redirect('/visitas');
-        })
+  return visitaService.updateVisita(id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
+    .then(visita => {
+      res.redirect('/visitas')
+    })
 })
 
 router.delete('/', (req, res) => {
-    const { id } = req.body;
-    visitaService.deleteVisita(id)
-        .then(() => {
-            res.redirect('/visitas')
-        });
-});
+  const { id } = req.body
+  visitaService.deleteVisita(id)
+    .then(() => {
+      res.redirect('/visitas')
+    })
+})
 
-module.exports = router;
+module.exports = router
--- services/visita.js
+++ services/visita.js
@@ -1,54 +1,59 @@
 const models = require('../models');
+const { paginateModel } = require('./utils')
 
 module.exports = {
-    getVisitas(args){
-        return models.Visita.findAll({
-            where:{
-                ...args
-            },
-            include: [{model: models.Cliente, include: models.Persona}, {model: models.Guia, include: models.Persona}, {model: models.Exhibicion}]
-        });
-    },
-    getVisita( id ){
-        return models.Visita.findOne({
-            where:{
-                id
-            },
-            include: [{model: models.Cliente, include: models.Persona}, {model: models.Guia, include: models.Persona}, {model: models.Exhibicion}]
-        });
-    },
-    createVisita(ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio){
-        return models.Visita.create({
-            ExhibicionId,
-            ClienteId,
-            GuiumId,
-            cantidadDePersonas,
-            fechaVisita,
-            horario,
-            precio,
-            cancelada:false
+  countVisitas() {
+    return models.Visita.count()
+  },
+  getVisitas(page = 0, pageSize = 10, args) {
+    return models.Visita.findAndCountAll({
+      where: {
+        ...args
+      },
+      include: [{ model: models.Cliente, include: models.Persona }, { model: models.Guia, include: models.Persona }, { model: models.Exhibicion }],
+      ...paginateModel({page, pageSize})
+    });
+  },
+  getVisita(id) {
+    return models.Visita.findOne({
+      where: {
+        id
+      },
+      include: [{ model: models.Cliente, include: models.Persona }, { model: models.Guia, include: models.Persona }, { model: models.Exhibicion }]
+    });
+  },
+  createVisita(ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio) {
+    return models.Visita.create({
+      ExhibicionId,
+      ClienteId,
+      GuiumId,
+      cantidadDePersonas,
+      fechaVisita,
+      horario,
+      precio,
+      cancelada: false
+    })
+  },
+  updateVisita(id, ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio, cancelada = false) {
+    return models.Visita.findByPk(id)
+      .then(visit => {
+        return visit.update({
+          ExhibicionId,
+          ClienteId,
+          GuiumId,
+          cantidadDePersonas,
+          fechaVisita,
+          horario,
+          precio,
+          cancelada
         })
-    },
-    updateVisita(id, ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio, cancelada = false){
-        return models.Visita.findByPk(id)
-            .then(visit => {
-                return visit.update({
-                    ExhibicionId,
-                    ClienteId,
-                    GuiumId,
-                    cantidadDePersonas,
-                    fechaVisita,
-                    horario,
-                    precio,
-                    cancelada                    
-                })
-            });
-    },
-    deleteVisita(id){
-        return models.Visita.findByPk(id)
-            .then( (visita) => {
-                visita.destroy();
-            })
-            
-    }
+      });
+  },
+  deleteVisita(id) {
+    return models.Visita.findByPk(id)
+      .then((visita) => {
+        visita.destroy();
+      })
+
+  }
 }
