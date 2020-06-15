b92dbcd4d3d2e05a7492874227eed1413f3dafb1
danibarria <barriareyesjosedaniel@gmail.com>
2020-06-15 16:01:09 -0300

    rol unico

--- models/core/rol.js
+++ models/core/rol.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize,DataTypes) =>{
     const Rol = sequelize.define('Rol',{
-        descripcion: DataTypes.STRING,
+        descripcion: {
+            type: DataTypes.STRING,
+            allowNull : false,
+			unique: true
+        },
         fecha_fin: DataTypes.DATEONLY
     },{
         paranoid:true
