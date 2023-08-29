53e519df90fa1a88c0137c359a6c9b6ddd1c6f0b
danibarria <barriareyesjosedaniel@gmail.com>
2023-08-29 19:40:57 -0300

    arreglado clientes

--- services/cliente.js
+++ services/cliente.js
@@ -1,61 +1,43 @@
-const models = require("../models");
-const personaService = require("./persona");
-const { paginateModel } = require('./utils')
+const models = require('../models');
+const personaService = require('./persona');
+const { paginateModel } = require('./utils');
 
 module.exports = {
-  getAllClientes(args){
+  getAllClientes(args, opts = {}) {
     return models.Cliente.findAll({
       include: [models.Persona],
       where: {
-        ...args
-      }
-    })
+        ...args,
+      },
+      ...opts,
+    });
   },
   getClientes(page = 0, pageSize = 10, args) {
     //{ tags }//aca se pide datos a la BD        //Cambia ya que no existe rol solo cliente
     return models.Cliente.findAndCountAll({
       include: [models.Persona],
       where: {
-        ...args
+        ...args,
       },
-      ...paginateModel({ page, pageSize })
+      ...paginateModel({ page, pageSize }),
     });
   },
-  getCliente(id) {
-    return models.Cliente.findByPk(id, { include: [models.Persona] });
+  getCliente(id, opts = {}) {
+    return models.Cliente.findByPk(id, { include: [models.Persona], ...opts });
   },
   createClienteExiste(tipo, PersonaId) {
     return models.Cliente.create({
       tipo,
-      PersonaId
+      PersonaId,
     });
   },
-  createCliente(
-    tipo,
-    identificacion,
-    nombre,
-    apellido,
-    direccion,
-    localidad,
-    email,
-    fecha_nacimiento,
-    telefono
-  ) {
+  createCliente(tipo, identificacion, nombre, apellido, direccion, localidad, email, fecha_nacimiento, telefono) {
     return personaService
-      .createPersona(
-        identificacion,
-        nombre,
-        apellido,
-        direccion,
-        localidad,
-        email,
-        fecha_nacimiento,
-        telefono
-      )
+      .createPersona(identificacion, nombre, apellido, direccion, localidad, email, fecha_nacimiento, telefono)
       .then(persona => {
         return models.Cliente.create({
           tipo,
-          PersonaId: persona.id
+          PersonaId: persona.id,
         });
       });
   },
@@ -66,5 +48,5 @@ module.exports = {
     return models.Cliente.findByPk(id).then(clienteEncontrado => {
       return clienteEncontrado.destroy(clienteEncontrado);
     });
-  }
+  },
 };
