511bdfe292900d7cbc0ac80d98976b8bb9f22710
CarlosIrigoyen <cyrigoyen969@gmail.com>
2024-02-11 17:34:50 -0300

    se avanzo con la paginacion en el server

--- services/dinosaurio.js
+++ services/dinosaurio.js
@@ -1,21 +1,38 @@
 const models = require('../models')
 const { paginateModel } = require('./utils')
-
+const { Op } = require('sequelize')
 module.exports = {
   getAllDinosaurios() {
     return models.Dinosaurio.findAll({
       include: [models.SubClase], raw: true, nest: true
     })
   },
+  getDinosauriosDataTable({ draw, start, length, search, order, columns }) {
+    let querySearch = undefined;
+    if (search.value && search.value.length>1) {
+      querySearch = { [Op.or]: [{ nombre: { [Op.iLike]: `%${search.value}%` } }] }
+    }
+    console.log('!!!!!!', start, length, querySearch)
+    return models.Dinosaurio.findAll({
+      offset: start,
+      limit: length,
+      where: querySearch,
+      include: [models.SubClase], raw: true, nest: true, logging: console.log
+    })
+  },
+  countDinosaurios() {
+    return models.Dinosaurio.count()
+  },
   getDinosaurios(page = 0, pageSize = 10, args) {
     return models.Dinosaurio.findAndCountAll({
-      include: [models.SubClase], 
+      include: [models.SubClase],
       where: {
         ...args
       },
       ...paginateModel({ page, pageSize })
     })
   },
+
   getDinosaurio(id) {
     return models.Dinosaurio.findByPk(id, {
       include: [models.SubClase], raw: true, nest: true
