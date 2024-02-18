73c0f0152370c2e7c7aa4e81a385759ddcc2fcd6
CarlosIrigoyen <cyrigoyen969@gmail.com>
2024-02-18 16:56:20 -0300

    realizada la paginacion y la busqueda en fosiles

--- routes/fosiles.js
+++ routes/fosiles.js
@@ -1,60 +1,73 @@
-const express = require('express')
-const permisos = require('../middlewares/permisos')
-const router = express.Router()
+const express = require('express');
+const permisos = require('../middlewares/permisos');
+const router = express.Router();
 
-const dinoService = require('../services/dinosaurio')
-const fosilService = require('../services/fosil')
+const dinoService = require('../services/dinosaurio');
+const fosilService = require('../services/fosil');
 
-const bones = ['Cráneo', 'Torax', 'Vertebral', 'Pelvis', 'Brazo', 'Manos', 'Piernas', 'Pies']
-const { generatePagination } = require('../services/utils')
-const paginate = require('../middlewares/paginate')
+const bones = ['Cráneo', 'Torax', 'Vertebral', 'Pelvis', 'Brazo', 'Manos', 'Piernas', 'Pies'];
+const { generatePagination } = require('../services/utils');
+const paginate = require('../middlewares/paginate');
 
-router.get('/',
-  async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const fosiles = await fosilService.getAllFosiles();
+    res.render('fosiles/fosil', { results: fosiles, req });
+  } catch (error) {
+    res.redirect('/404');
+  }
+}),
+  router.get('/list', async (req, res) => {
     try {
-      const fosiles = await fosilService.getAllFosiles()
-      res.render('fosiles/fosil', { results: fosiles, req })
+      const total = await fosilService.countFosiles();
+      const { start, length, draw, search, columns, order } = req.query;
+      console.log('RUTA....', { search });
+      const fosiles = await fosilService.getFosilesDataTable({ start, length, search, columns, order });
+      res.json({ draw, data: fosiles, recordsTotal: total, recordsFiltered: total });
     } catch (error) {
-      res.redirect('/404')
+      res.redirect('/404');
     }
-  })
+  });
 
-router.get('/agregar',
-  async (req, res) => {
-    const results= await dinoService.getAllDinosaurios()
-    res.render('fosiles/agregar', { results, bones, req })
-  })
+router.get('/agregar', async (req, res) => {
+  const results = await dinoService.getAllDinosaurios();
+  res.render('fosiles/agregar', { results, bones, req });
+});
 
-router.get('/editar/:id',
-  async (req, res) => {
-    const { id } = req.params
-    const fosil = await fosilService.getFosil(id)
-    const dinosaurios = await dinoService.getAllDinosaurios()
-    const dinosaurio = fosil.Dinosaurio
-    const bonesFiltered = bones.filter(bone => bone !== fosil.huesos)
-    res.render('fosiles/editar', { dinosaurio, dinosaurios, bones: bonesFiltered, fosil, req })
-  })
+router.get('/editar/:id', async (req, res) => {
+  const { id } = req.params;
+  const fosil = await fosilService.getFosil(id);
+  const dinosaurios = await dinoService.getAllDinosaurios();
+  const dinosaurio = fosil.Dinosaurio;
+  const bonesFiltered = bones.filter(bone => bone !== fosil.huesos);
+  res.render('fosiles/editar', { dinosaurio, dinosaurios, bones: bonesFiltered, fosil, req });
+});
 
+router.put('/', async (req, res) => {
+  await fosilService.updateFosil(req.body);
+  res.redirect('/fosiles');
+});
 
-router.put('/',
-  async (req, res) => {
-    await fosilService.updateFosil(req.body) 
-    res.redirect('/fosiles')
-  })
+router.post('/', async (req, res) => {
+  const { DinosaurioId, huesos, numero_coleccion, peso, disponible, fecha_encontrado, observacion } = req.body;
+  try {
+    // createFosil(numero_coleccion, peso, disponible, fecha_encontrado, observacion, DinosaurioId, huesos) {
+    const fosil = await fosilService.createFosil(
+      numero_coleccion,
+      peso,
+      disponible,
+      fecha_encontrado,
+      observacion,
+      DinosaurioId,
+      huesos
+    );
+    res.redirect('/fosiles');
+  } catch (error) {
+    /** @TODO revisar */
+    const { message } = error.errors[0];
+    const results = await dinoService.getAllDinosaurios();
+    res.render('fosiles/agregar', { results, dino: req.body, bones, req, errores: message });
+  }
+});
 
-router.post('/',
-  async (req, res) => {
-    const { DinosaurioId, huesos, numero_coleccion, peso, disponible, fecha_encontrado, observacion } = req.body
-    try {
-      // createFosil(numero_coleccion, peso, disponible, fecha_encontrado, observacion, DinosaurioId, huesos) {
-      const fosil = await fosilService.createFosil(numero_coleccion, peso, disponible, fecha_encontrado, observacion, DinosaurioId, huesos)
-      res.redirect('/fosiles')
-    } catch (error) {
-      /** @TODO revisar */
-      const { message } = error.errors[0]
-      const results = await dinoService.getAllDinosaurios()
-      res.render('fosiles/agregar', { results, dino: req.body, bones, req, errores: message })
-    }
-  })
-
-module.exports = router
+module.exports = router;
