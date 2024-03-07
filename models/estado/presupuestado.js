9b97356321a6a1943a68cdccda57d2be8461e702
FrancoDeMarco <@FrancoDeMarco.com>
2024-03-07 20:43:20 -0300

    Ya funciona el Cancelar Pedidos

--- models/estado/presupuestado.js
+++ models/estado/presupuestado.js
@@ -1,76 +1,76 @@
 'use strict'
 const Sequelize = require('sequelize');
 
-module.exports = (sequelize, DataTypes) =>{
-    class Presupuestado extends Sequelize.Model{
-        cancelar(pedido, args){
+module.exports = (sequelize, DataTypes) => {
+    class Presupuestado extends Sequelize.Model {
+        async cancelar(pedido, args) {
             const PedidoId = pedido.id;
-            return sequelize.models.Cancelado.create({
+            const cancelado = await sequelize.models.Cancelado.create({
                 PedidoId,
-                obs:args.observacion,
-                fecha:this.fecha_fin_oferta,
+                obs: args.observacion,
+                fecha: new Date(),
                 fecha_baja: new Date()
-            }).then(()=>{
-                return pedido.update({
-                    autorizacion:true
-                })
             })
+            await pedido.update({
+                autorizacion: true
+            })
+            return cancelado
         }
-        facturar(pedido, args){
+        facturar(pedido, args) {
             const PedidoId = pedido.id;
             return sequelize.models.Facturado.create({
                 PedidoId
-            }).then(()=>{
+            }).then(() => {
                 //actualiza el estado
                 return sequelize.models.Pago.create({
-                    tipo:args.tipopago,
+                    tipo: args.tipopago,
                     monto: args.presupuesto,
                     moneda: this.moneda
                 })
-            }).then(() =>{
+            }).then(() => {
                 return pedido.update({
-                    autorizacion:true,
-                })  
-            }).then(()=>{
+                    autorizacion: true,
+                })
+            }).then(() => {
                 return sequelize.models.Confirmado.create({
                     PedidoId,
-                    fecha:new Date()
+                    fecha: new Date()
                 })
             })
         }
-   }
-   Presupuestado.init({
-    descripcion: {
-        type:DataTypes.STRING,
-        defaultValue:'Presupuestado'
-    },
-    fecha:{
-        type: DataTypes.DATE,
-        defaultValue: new Date(),
-        allowNull:false
-    },
-    cantidad_huesos: DataTypes.INTEGER,
-    monto: DataTypes.FLOAT,
-    moneda: {
-        type: DataTypes.ENUM,
-        values: ['Pesos Argentinos', 'Dolares','Euros'] 
-    }, 
-    fecha_fin_oferta: DataTypes.DATEONLY,
-    PedidoId: {
-        type:DataTypes.INTEGER,
-        references:{
-            model:'Pedidos',
-            key:'id'
-        }
     }
-   }, {
-       paranoid:true,
-       sequelize
+    Presupuestado.init({
+        descripcion: {
+            type: DataTypes.STRING,
+            defaultValue: 'Presupuestado'
+        },
+        fecha: {
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: false
+        },
+        cantidad_huesos: DataTypes.INTEGER,
+        monto: DataTypes.FLOAT,
+        moneda: {
+            type: DataTypes.ENUM,
+            values: ['Pesos Argentinos', 'Dolares', 'Euros']
+        },
+        fecha_fin_oferta: DataTypes.DATEONLY,
+        PedidoId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Pedidos',
+                key: 'id'
+            }
+        }
+    }, {
+        paranoid: true,
+        sequelize
     });
-   Presupuestado.associate = function (models){
+    Presupuestado.associate = function (models) {
         Presupuestado.belongsTo(models.Pedido);
-   }
-   return Presupuestado;
+    }
+    return Presupuestado;
 }
 
 // asignarHuesos(colHuesos)
