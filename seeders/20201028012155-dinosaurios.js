120737f397885362bf6ebc6dd922d5f99109c0f6
CarlosIrigoyen <cyrigoyen969@gmail.com>
2024-07-05 18:56:23 -0300

    se  creo la seeds de exhibiciones y de visitas guiadas

--- seeders/20201028012155-dinosaurios.js
+++ seeders/20201028012155-dinosaurios.js
@@ -13,27 +13,36 @@ const torso = ['Costillas Cervicales', 'Costillas Dorsales'];
 
 const base = ['Pelvis', 'Brazo', 'Piernas', 'Pies', 'Manos', 'Vertebras', 'Torso'];
 
-const huesosPersonalizados = ['Vertebras Cervicales', 'Vertebras Dorsales', 'Vertebras Sacras', 'Vertebras Caudales', 'Costillas Cervicales', 'Costillas Dorsales', 'Hemales', 'Metacarpianos', 'Metatarsales', 'Dedos Mano', 'Dedos Pie'];
+const huesosPersonalizados = [
+  'Vertebras Cervicales',
+  'Vertebras Dorsales',
+  'Vertebras Sacras',
+  'Vertebras Caudales',
+  'Costillas Cervicales',
+  'Costillas Dorsales',
+  'Hemales',
+  'Metacarpianos',
+  'Metatarsales',
+  'Dedos Mano',
+  'Dedos Pie',
+];
 
 const apendiculares = pelvis.concat(brazo).concat(piernas);
 
-
 const faker = require('faker/locale/es_MX');
-
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-
     let dinosaurioArr = [];
-    const definirTipoSubtipo = (nombre) => {
+    const definirTipoSubtipo = nombre => {
       switch (nombre) {
         case 'Paladar':
         case 'Mandíbula':
         case 'Cráneo':
           return {
             tipohueso: 'Axial',
-            subtipohueso: 'Cráneo'
-          }
+            subtipohueso: 'Cráneo',
+          };
           break;
         case 'Coracoide':
         case 'Ilion':
@@ -42,8 +51,8 @@ module.exports = {
         case 'Pelvis':
           return {
             tipohueso: 'Apendicular',
-            subtipohueso: 'Pelvis'
-          }
+            subtipohueso: 'Pelvis',
+          };
           break;
         case 'Radio':
         case 'Unla':
@@ -52,8 +61,8 @@ module.exports = {
         case 'Brazo':
           return {
             subtipohueso: 'Brazo',
-            tipohueso: 'Apendicular'
-          }
+            tipohueso: 'Apendicular',
+          };
           break;
         case 'Fémur':
         case 'Tibia':
@@ -61,24 +70,24 @@ module.exports = {
         case 'Piernas':
           return {
             subtipohueso: 'Piernas',
-            tipohueso: 'Apendicular'
-          }
+            tipohueso: 'Apendicular',
+          };
           break;
         case 'Metatarsales':
         case 'Dedos Pie':
         case 'Pies':
           return {
             subtipohueso: 'Pies',
-            tipohueso: 'Apendicular'
-          }
+            tipohueso: 'Apendicular',
+          };
           break;
         case 'Metacarpianos':
         case 'Dedos Mano':
         case 'Manos':
           return {
             tipohueso: 'Apendicular',
-            subtipohueso: 'Manos'
-          }
+            subtipohueso: 'Manos',
+          };
           break;
         case 'Hemales':
         case 'Vertebras Cervicales':
@@ -89,8 +98,8 @@ module.exports = {
         case 'Vertebras':
           return {
             subtipohueso: 'Vertebral',
-            tipohueso: 'Axial'
-          }
+            tipohueso: 'Axial',
+          };
           break;
         case 'Costillas Cervicales':
         case 'Costillas Dorsales':
@@ -98,50 +107,46 @@ module.exports = {
         case 'Torso':
           return {
             subtipohueso: 'Torax',
-            tipohueso: 'Axial'
-          }
+            tipohueso: 'Axial',
+          };
           break;
       }
-    }
+    };
     const generarSubclase = () => {
       return Math.round(Math.random() * 6) + 1;
-    }
+    };
     const generarAlimentacion = () => {
-      const alimentacion = ['Herbivoro', 'Carnivoro', 'Omnivoro']
+      const alimentacion = ['Herbivoro', 'Carnivoro', 'Omnivoro'];
       return alimentacion[Math.round(Math.random() * 2)];
-    }
+    };
     const generarPeriodo = () => {
-      const periodo = ['Cretacico', 'Jurasico', 'Triasico']
+      const periodo = ['Cretacico', 'Jurasico', 'Triasico'];
       return periodo[Math.round(Math.random() * 2)];
-    }
+    };
 
-    let huesosArr = []
-
-
+    let huesosArr = [];
 
     //**En esta parte se crean los dinos */
 
     const nombresDinosaurios = [
-      "Ankylosaurus",
-      "Brachiosaurus",
-      "Stegosaurus",
-      "Pterodactyl",
-      "Allosaurus",
-      "Diplodocus",
-      "Parasaurolophus",
-      "Spinosaurus",
-      "Velociraptor",
-      "Triceratops",
-      "Tyrannosaurus",
-      "Archaeopteryx",
-      "Iguanodon",
-      "Microraptor",
-      "Oviraptor",
+      'Ankylosaurus',
+      'Brachiosaurus',
+      'Stegosaurus',
+      'Pterodactyl',
+      'Allosaurus',
+      'Diplodocus',
+      'Parasaurolophus',
+      'Spinosaurus',
+      'Velociraptor',
+      'Triceratops',
+      'Tyrannosaurus',
+      'Archaeopteryx',
+      'Iguanodon',
+      'Microraptor',
+      'Oviraptor',
     ];
 
     for (let index = 1; index <= nombresDinosaurios.length + 1; index++) {
-
-
       let dinosaurioObj = {
         nombre: nombresDinosaurios[index - 1],
         alimentacion: generarAlimentacion(),
@@ -151,9 +156,9 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         deletedAt: null,
-      }
+      };
 
-      dinosaurioArr.push(dinosaurioObj)
+      dinosaurioArr.push(dinosaurioObj);
 
       /*  Estos son los objetos dinosaurio que tendría que probar después. Por ahora lo voy a dejar como está
           
@@ -347,22 +352,23 @@ module.exports = {
           tipohueso: definirTipoSubtipo(nombre).tipohueso,
           subtipohueso: definirTipoSubtipo(nombre).subtipohueso,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         });
-
-      })
-      craneo.forEach((nombre) => {
+      });
+      craneo.forEach(nombre => {
         huesosArr.push({
-          nombre, numero: 1,
+          nombre,
+          numero: 1,
           DinosaurioId: index,
           tipohueso: definirTipoSubtipo(nombre).tipohueso,
           subtipohueso: definirTipoSubtipo(nombre).subtipohueso,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         });
       });
-      for (var i = 1; i <= 2; i++) { //los que tienen solo dos    
-        apendiculares.forEach((nombre) => {
+      for (var i = 1; i <= 2; i++) {
+        //los que tienen solo dos
+        apendiculares.forEach(nombre => {
           huesosArr.push({
             nombre,
             numero: i,
@@ -370,11 +376,12 @@ module.exports = {
             tipohueso: definirTipoSubtipo(nombre).tipohueso,
             subtipohueso: definirTipoSubtipo(nombre).subtipohueso,
             createdAt: new Date(),
-            updatedAt: new Date()
+            updatedAt: new Date(),
           });
         });
       }
-      for (const key in [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) {//agrega por cada elemento de la lista huesos personalizados, la cantidad que llega para cada indice
+      for (const key in [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) {
+        //agrega por cada elemento de la lista huesos personalizados, la cantidad que llega para cada indice
         for (var j = 1; j <= [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1][key]; j++) {
           huesosArr.push({
             nombre: huesosPersonalizados[key],
@@ -383,20 +390,17 @@ module.exports = {
             tipohueso: definirTipoSubtipo(huesosPersonalizados[key]).tipohueso,
             subtipohueso: definirTipoSubtipo(huesosPersonalizados[key]).subtipohueso,
             createdAt: new Date(),
-            updatedAt: new Date()
-          })
+            updatedAt: new Date(),
+          });
         }
       }
-
     }
-    return queryInterface.bulkInsert('Dinosaurios', dinosaurioArr, {})
-      .then(() => {
-        return queryInterface.bulkInsert('Huesos', huesosArr, {});
-      })
+    return queryInterface.bulkInsert('Dinosaurios', dinosaurioArr, {}).then(() => {
+      return queryInterface.bulkInsert('Huesos', huesosArr, {});
+    });
   },
-
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Dinosaurios', null, {});
-  }
+  },
 };
---
+++ seeders/visitas.js
@@ -0,0 +1,171 @@
+'use strict';
+const faker = require('faker/locale/es_MX');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Datos de ejemplo para las exhibiciones centradas en dinosaurios y épocas prehistóricas
+    const exhibicionesData = [
+      {
+        nombre: 'Fósiles de Dinosaurios',
+        duracion: 6,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Replicas de Dinosaurios',
+        duracion: 9,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Exhibición de Paleontología',
+        duracion: 12,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Mundo Jurásico',
+        duracion: 7,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Evolución de los Dinosaurios',
+        duracion: 6,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Fósiles y Rastros de Dinosaurios',
+        duracion: 9,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Dinosaurios en la Historia',
+        duracion: 8,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Museo de Paleontología',
+        duracion: 10,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Rastros del Pasado',
+        duracion: 6,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Vida en el Jurásico',
+        duracion: 8,
+        tematica: 'Dinosaurios y Épocas Prehistóricas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    // Insertar los datos en la tabla Exhibicions
+    await queryInterface.bulkInsert('Exhibicions', exhibicionesData, {});
+
+    // Continuación con la creación de visitas
+    let visitasArr = [];
+    let today = new Date();
+    let startDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+    let daysInYear = 365;
+    let totalVisits = 300; // número de visitas que se desea crear
+
+    // Distribución de tipos de visitas
+    let schoolVisits = Math.round(totalVisits * 0.54);
+    let universityVisits = Math.round(totalVisits * 0.08);
+    let privateVisits = Math.round(totalVisits * 0.12);
+    let govOrganizationVisits = totalVisits - (schoolVisits + universityVisits + privateVisits);
+
+    // Función para generar una fecha aleatoria entre el rango especificado
+    const generateRandomDate = (start, end) => {
+      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    };
+
+    // Generar visitas
+    for (let i = 0; i < totalVisits; i++) {
+      let visitaFecha;
+      let visitaEstado;
+      let visitaTipo;
+      let exhibicionId = faker.random.number({ min: 1, max: exhibicionesData.length });
+
+      // Asignar tipo de visita siguiendo una distribución
+      if (schoolVisits > 0) {
+        visitaTipo = 'Escuela';
+        schoolVisits--;
+      } else if (universityVisits > 0) {
+        visitaTipo = 'Universidad';
+        universityVisits--;
+      } else if (privateVisits > 0) {
+        visitaTipo = 'Particular';
+        privateVisits--;
+      } else {
+        visitaTipo = 'Organización Gubernamental';
+        govOrganizationVisits--;
+      }
+
+      do {
+        visitaFecha = generateRandomDate(startDate, today);
+        visitaEstado = faker.random.arrayElement(['Pendiente', 'Cancelada', 'Completada']);
+      } while (
+        visitaEstado === 'Pendiente' &&
+        visitasArr.some(v => v.fechaVisita.toDateString() === visitaFecha.toDateString() && v.estado === 'Pendiente')
+      );
+
+      let visitaObj = {
+        cantidadDePersonas: faker.random.number({ min: 1, max: 100 }),
+        fechaVisita: visitaFecha,
+        horario: faker.random.arrayElement([
+          '9:00hs',
+          '10:00hs',
+          '11:00hs',
+          '12:00hs',
+          '13:00hs',
+          '14:00hs',
+          '15:00hs',
+          '16:00hs',
+          '17:00hs',
+          '18:00hs',
+        ]),
+        precio: faker.random.number({ min: 1, max: 3000000 }),
+        estado: visitaEstado,
+        observacion: visitaTipo === 'Particular' ? null : `Visita solicitada por ${visitaTipo}`,
+        cancelada: visitaEstado === 'Cancelada',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        ClienteId: faker.random.number({ min: 1, max: 20 }),
+        GuiumId: faker.random.number({ min: 1, max: 20 }),
+        ExhibicionId: faker.random.number({ min: 1, max: 10 }),
+      };
+
+      visitasArr.push(visitaObj);
+    }
+
+    // Insertar los datos en la tabla Visitas
+    return queryInterface.bulkInsert('Visita', visitasArr, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar todos los datos de la tabla Visitas
+    await queryInterface.bulkDelete('Visita', null, {});
+
+    // Eliminar todos los datos de la tabla Exhibicions
+    return queryInterface.bulkDelete('Exhibicion', null, {});
+  },
+};
