1b53eff1288b019bc98eb4c16dffaa3db3b89865
CarlosIrigoyen <cyrigoyen969@gmail.com>
2024-07-22 18:33:06 -0300

    se agrego el control del horario de la visita al editar

--- routes/visitas.js
+++ routes/visitas.js
@@ -1,70 +1,83 @@
-const express = require('express')
-const router = express.Router()
-const visitaService = require('../services/visita')
-const exhibicionService = require('../services/exhibicion')
-const clienteService = require('../services/cliente')
-const guiaService = require('../services/guia')
+const express = require('express');
+const router = express.Router();
+const visitaService = require('../services/visita');
+const exhibicionService = require('../services/exhibicion');
+const clienteService = require('../services/cliente');
+const guiaService = require('../services/guia');
 
-const paginate = require('../middlewares/paginate')
-const { generatePagination } = require("../services/utils")
+const paginate = require('../middlewares/paginate');
+const { generatePagination } = require('../services/utils');
 
 router.get('/', async (req, res) => {
   try {
-    const visitas = await visitaService.getAllVisitas({},{raw:true,nest:true})
-    res.render('visitas/visita', { visitas, req })
+    const visitas = await visitaService.getAllVisitas({}, { raw: true, nest: true });
+    res.render('visitas/visita', { visitas, req });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 router.get('/agregar', async (req, res) => {
-  const exhibiciones = await exhibicionService.getAllExhibiciones({},{raw:true,nest:true})
-  const clientes = await clienteService.getAllClientes({},{raw:true,nest:true})
-  const guias = await guiaService.getAllGuias()
-  res.render('visitas/agregar', { exhibiciones, clientes, guias, req })
-})
-
+  const exhibiciones = await exhibicionService.getAllExhibiciones({}, { raw: true, nest: true });
+  const clientes = await clienteService.getAllClientes({}, { raw: true, nest: true });
+  const guias = await guiaService.getAllGuias();
+  res.render('visitas/agregar', { exhibiciones, clientes, guias, req });
+});
+router.get('/validar', async (req, res) => {
+  const { fecha } = req.query;
+  if (!fecha) {
+    return res.status(400).send('Fecha no proporcionada');
+  }
+  try {
+    const horariosDisponibles = await visitaService.verificarVisitas(fecha);
+    console.log(horariosDisponibles);
+    res.json(horariosDisponibles);
+  } catch (error) {
+    console.error('Error al validar horarios:', error);
+    res.status(500).send('Error al validar horarios');
+  }
+});
 router.get('/editar/:id', async (req, res) => {
-  const { id } = req.params
-  const exhibiciones = await exhibicionService.getAllExhibiciones({},{raw:true,nest:true})
-  const clientes = await clienteService.getAllClientes({},{raw:true,nest:true})
-  const guias = await guiaService.getAllGuias()
-  const visita = await visitaService.getVisita(id, {raw:true,nest:true})
-  res.render('visitas/editar', { visita, exhibiciones, clientes, guias, req })
-})
+  const { id } = req.params;
+  const exhibiciones = await exhibicionService.getAllExhibiciones({}, { raw: true, nest: true });
+  const clientes = await clienteService.getAllClientes({}, { raw: true, nest: true });
+  const guias = await guiaService.getAllGuias();
+  const visita = await visitaService.getVisita(id, { raw: true, nest: true });
+  res.render('visitas/editar', { visita, exhibiciones, clientes, guias, req });
+});
 
 router.get('/eliminar/:id', async (req, res) => {
-  const { id } = req.params
-  const visita = await visitaService.getVisita(id, {raw:true,nest:true})
-  res.render('visitas/eliminar', { visita, req })
-})
+  const { id } = req.params;
+  const visita = await visitaService.getVisita(id, { raw: true, nest: true });
+  res.render('visitas/eliminar', { visita, req });
+});
 
 router.post('/', async (req, res) => {
-  const { exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body
+  const { exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body;
   /** @TODO agregar try catch y la vista de agregar visita */
   try {
-    await visitaService.createVisita(exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
-    res.redirect('/visitas')
+    await visitaService.createVisita(exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio);
+    res.redirect('/visitas');
   } catch (error) {
-    res.render('visitas/agregar', { req })    
+    res.render('visitas/agregar', { req });
   }
-})
+});
 
 router.put('/', async (req, res) => {
-  const { id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body
+  const { id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio } = req.body;
   /** agregar async await, try catch, render con visitas, request, error */
-  // ver que onda visita no se usa 
-  return visitaService.updateVisita(id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
+  // ver que onda visita no se usa
+  return visitaService
+    .updateVisita(id, exhibicionId, clienteId, guiaId, cantidadPersonas, fecha, horario, precio)
     .then(visita => {
-      res.redirect('/visitas')
-    })
-})
+      res.redirect('/visitas');
+    });
+});
 
 router.delete('/', async (req, res) => {
-  const { id } = req.body
-  await visitaService.deleteVisita(id)
-  res.redirect('/visitas')
-    
-})
+  const { id } = req.body;
+  await visitaService.deleteVisita(id);
+  res.redirect('/visitas');
+});
 
-module.exports = router
+module.exports = router;
--- services/visita.js
+++ services/visita.js
@@ -1,35 +1,87 @@
 const models = require('../models');
-const { paginateModel } = require('./utils')
+const { paginateModel } = require('./utils');
 
 module.exports = {
-  getAllVisitas(args,opts={}){
+  getAllVisitas(args, opts = {}) {
     return models.Visita.findAll({
       where: {
-        ...args
+        ...args,
       },
-      include: [{ model: models.Cliente, include: models.Persona }, { model: models.Guia, include: models.Persona }, { model: models.Exhibicion }],
-      ...opts
+      include: [
+        { model: models.Cliente, include: models.Persona },
+        { model: models.Guia, include: models.Persona },
+        { model: models.Exhibicion },
+      ],
+      ...opts,
     });
   },
+  async verificarVisitas(fecha) {
+    try {
+      // Convertir la fecha al formato ISO (YYYY-MM-DD)
+      const fechaISO = new Date(fecha).toISOString().split('T')[0];
+
+      // Consultar todas las visitas no canceladas para la fecha proporcionada
+      const visitas = await models.Visita.findAll({
+        where: {
+          fechaVisita: fechaISO,
+          cancelada: false,
+        },
+      });
+
+      // Horarios disponibles inicialmente de 9 a 18 hs (asumiendo horas completas)
+      const horariosDisponibles = [
+        '09:00hs',
+        '10:00hs',
+        '11:00hs',
+        '12:00hs',
+        '13:00hs',
+        '14:00hs',
+        '15:00hs',
+        '16:00hs',
+        '17:00hs',
+        '18:00hs',
+      ];
+
+      // Extraer los horarios ocupados
+      const horariosOcupados = visitas.map(visita => visita.horario);
+
+      // Filtrar los horarios disponibles para excluir los ocupados
+      const horariosFinales = horariosDisponibles.filter(horario => !horariosOcupados.includes(horario));
+
+      // Devolver los horarios disponibles
+      return horariosFinales;
+    } catch (error) {
+      console.error('Error al verificar visitas:', error);
+      return []; // Devuelve una lista vacía en caso de error
+    }
+  },
   countVisitas() {
-    return models.Visita.count()
+    return models.Visita.count();
   },
   getVisitas(page = 0, pageSize = 10, args) {
     return models.Visita.findAndCountAll({
       where: {
-        ...args
+        ...args,
       },
-      include: [{ model: models.Cliente, include: models.Persona }, { model: models.Guia, include: models.Persona }, { model: models.Exhibicion }],
-      ...paginateModel({page, pageSize})
+      include: [
+        { model: models.Cliente, include: models.Persona },
+        { model: models.Guia, include: models.Persona },
+        { model: models.Exhibicion },
+      ],
+      ...paginateModel({ page, pageSize }),
     });
   },
-  getVisita(id, opts={}) {
+  getVisita(id, opts = {}) {
     return models.Visita.findOne({
       where: {
-        id
+        id,
       },
       ...opts,
-      include: [{ model: models.Cliente, include: models.Persona }, { model: models.Guia, include: models.Persona }, { model: models.Exhibicion }]
+      include: [
+        { model: models.Cliente, include: models.Persona },
+        { model: models.Guia, include: models.Persona },
+        { model: models.Exhibicion },
+      ],
     });
   },
   createVisita(ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio) {
@@ -41,29 +93,36 @@ module.exports = {
       fechaVisita,
       horario,
       precio,
-      cancelada: false
-    })
+      cancelada: false,
+    });
   },
-  updateVisita(id, ExhibicionId, ClienteId, GuiumId, cantidadDePersonas, fechaVisita, horario, precio, cancelada = false) {
-    return models.Visita.findByPk(id)
-      .then(visit => {
-        return visit.update({
-          ExhibicionId,
-          ClienteId,
-          GuiumId,
-          cantidadDePersonas,
-          fechaVisita,
-          horario,
-          precio,
-          cancelada
-        })
+  updateVisita(
+    id,
+    ExhibicionId,
+    ClienteId,
+    GuiumId,
+    cantidadDePersonas,
+    fechaVisita,
+    horario,
+    precio,
+    cancelada = false
+  ) {
+    return models.Visita.findByPk(id).then(visit => {
+      return visit.update({
+        ExhibicionId,
+        ClienteId,
+        GuiumId,
+        cantidadDePersonas,
+        fechaVisita,
+        horario,
+        precio,
+        cancelada,
       });
+    });
   },
   deleteVisita(id) {
-    return models.Visita.findByPk(id)
-      .then((visita) => {
-        visita.destroy();
-      })
-
-  }
-}
+    return models.Visita.findByPk(id).then(visita => {
+      visita.destroy();
+    });
+  },
+};
