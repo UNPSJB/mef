cf6314b6b8c9d313f49d77556a4ef514e40f7bbc
FrancoDeMarco <@FrancoDeMarco.com>
2024-08-27 19:42:40 -0300

    Tratando de hacer los servicios y rutas de los reportes de Visita.

---
+++ services/reportesVisitas.js
@@ -0,0 +1,10 @@
+function obtenerReportesSimulados() {
+    return [
+        { titulo: "Reporte 1", descripcion: "Descripción del reporte 1", fecha: "01/01/2024" },
+        { titulo: "Reporte 2", descripcion: "Descripción del reporte 2", fecha: "02/01/2024" }
+    ];
+}
+
+module.exports = {
+    obtenerReportesSimulados
+};
